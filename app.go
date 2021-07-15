9d8351f088b53d1a69e6c9d4ea6d124ce440ffdf
Chris Loper <chris.loper@passage.id>
2021-07-15 13:54:28 -0500

    Add functionality for ApiKeys:   - getUserInfo
    Change PubKey to request:
      - cache results in map[appHandle] --> rsa.PublicKey

--- app.go
+++ app.go
@@ -2,29 +2,81 @@ package passage
 
 import (
 	"crypto/rsa"
+	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
 type App struct {
-	Handle    string
-	PublicKey *rsa.PublicKey
+	handle    string
+	publicKey *rsa.PublicKey
 }
 
-func New() *App {
-	return &App{}
-}
+var apiKey string
+var publicKeyCache map[string]*rsa.PublicKey = make(map[string]*rsa.PublicKey)
 
-func (a *App) AuthenticateRequest(r *http.Request) (*User, error) {
-	// Check if the app's public key is set. If not, attempt to set it.
-	if a.PublicKey == nil {
-		pk, err := getPublicKeyFromEnv()
+func New(appHandle string, key ...string) (*App, error) {
+	if len(key) > 0 {
+		apiKey = key[0]
+	}
+	var pubKey *rsa.PublicKey
+	if _, ok := publicKeyCache[appHandle]; ok {
+		pubKey = publicKeyCache[appHandle]
+	} else {
+		var pubKey *rsa.PublicKey
+		//MAKE SOME REQUEST TO PASSAGE
+		resp, err := getAppInfo(appHandle)
 		if err != nil {
 			return nil, err
 		}
-		a.PublicKey = pk
+		pubKey, err = getRSAPublicKey(resp.PublicKey)
+		if err != nil {
+			return nil, err
+		}
+		publicKeyCache[appHandle] = pubKey
+	}
+	return &App{
+		handle:    appHandle,
+		publicKey: pubKey,
+	}, nil
+}
+
+type appInfoResp struct {
+	PublicKey string `json:"public_key"`
+}
+
+func getAppInfo(appHandle string) (*appInfoResp, error) {
+	client := http.DefaultClient
+	req, err := http.NewRequest("GET", "https://api.passage.id/v1/app/"+appHandle, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var retBody appInfoResp
+
+	jsonErr := json.Unmarshal(body, &retBody)
+	if jsonErr != nil {
+		return nil, err
+	}
+	return &retBody, nil
+}
+func (a *App) AuthenticateRequest(r *http.Request) (*User, error) {
+	// Check if the app's public key is set. If not, attempt to set it.
+	if a.publicKey == nil {
+		return nil, errors.New("public key never initializd in app Struct")
 	}
 
 	// Extract authentication token from the request.
@@ -38,7 +90,7 @@ func (a *App) AuthenticateRequest(r *http.Request) (*User, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, errors.New("invalid authentication token")
 		}
-		return a.PublicKey, nil
+		return a.publicKey, nil
 	})
 	if err != nil {
 		return nil, errors.New("invalid authentication token")
--- app_test.go
+++ app_test.go
@@ -1,11 +1,8 @@
-package passage_test
+package passage
 
 import (
 	"net/http"
-	"os"
 	"testing"
-
-	"github.com/passageidentity/passage-go"
 )
 
 func TestValidAuthenticateRequest(t *testing.T) {
@@ -18,10 +15,16 @@ func TestValidAuthenticateRequest(t *testing.T) {
 	// Add a VALID authentication token to the request
 	req.Header.Add("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwYXNzYWdlIiwic3ViIjoiQ2NGa0djWXdLTkxJcEFtWkpIdnQifQ.EdUKRJwgK7ICRm4uIfGcL3v8zZRyrPWm1zUg5bnQ0m2ovLPGk2yLARZfpE6xhYhd22aemBx8EtNvg6hBg3qOVxKgrAsL-V78Z7S6DKJRRxbKSfJlz55SZOfklo5pz_VwVtOE34oWlq9JRbFaFHxhJuST3Z9ykJEm-1Ihr9ACX453pX9iTcLJmVMWbT4AlPAPtAL4rNsAU9b-KDQC1FUlTNE5e7HfOAaSnwz9qQZpviDAHYomJcWxFCWMri4Zj6PVQb8l57nMUqhf2BU4_q5iVJpV7DlODwApkQIPNWCGZj3oqeJv4T0ldUivtgq1YzEdK4Cw6__13f2IRuDTBHFKsQ")
 
-	// Set a VALID public key as a properly configured server would
-	os.Setenv("PASSAGE_PUBLIC_KEY", "LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1JSUJDZ0tDQVFFQTVrZGlybnk1WnFjZ2NxcTg3YklMamtycFI3Ly9FRXRHR3lVL0xESkwxSnU2THRyaVNJemkKdzVPY2h4RkFKb25OUWhwd3dwKzNoT05OWWpKeFY1WWpibWlsc2ZMNWxsajJzZlJxY3lTMjBlZFZ2RDJqZ3hYWQpvT1g2SzU5dWtNQllzU0ZCcWtzM0UvYmNxd2YxN0U2bEIwVFhNY3NYUEtBNUE2QlJoMUVQSGtvQjlKWHJiS3hXCnhGT3JsRkJSRnJMVncvVmNUYnM3SXZRSGU4c3kxdlZoZGlkSng5R095MXVaQVlPL2dTcVNGNFRYd2RaaXJBQ1MKMjJFQlJFSSsrbWh1WWR3aWpaL3lZU1JwRFM4V0h1aVVMYjJrYnhVNUhoVG1jcnNrSjNjeEdnNHM1ZFA0V0YvZwpoMUNnYmdUczQ0dzFPVnUxdkNpNmMxQTQxMng3Wmh3UnR3SURBUUFCCi0tLS0tRU5EIFJTQSBQVUJMSUMgS0VZLS0tLS0K")
+	psg, err := New("UKbRUx")
+	if err != nil {
+		t.Error(err)
+	}
 
-	psg := passage.New()
+	pk, err := getRSAPublicKey("LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1JSUJDZ0tDQVFFQTVrZGlybnk1WnFjZ2NxcTg3YklMamtycFI3Ly9FRXRHR3lVL0xESkwxSnU2THRyaVNJemkKdzVPY2h4RkFKb25OUWhwd3dwKzNoT05OWWpKeFY1WWpibWlsc2ZMNWxsajJzZlJxY3lTMjBlZFZ2RDJqZ3hYWQpvT1g2SzU5dWtNQllzU0ZCcWtzM0UvYmNxd2YxN0U2bEIwVFhNY3NYUEtBNUE2QlJoMUVQSGtvQjlKWHJiS3hXCnhGT3JsRkJSRnJMVncvVmNUYnM3SXZRSGU4c3kxdlZoZGlkSng5R095MXVaQVlPL2dTcVNGNFRYd2RaaXJBQ1MKMjJFQlJFSSsrbWh1WWR3aWpaL3lZU1JwRFM4V0h1aVVMYjJrYnhVNUhoVG1jcnNrSjNjeEdnNHM1ZFA0V0YvZwpoMUNnYmdUczQ0dzFPVnUxdkNpNmMxQTQxMng3Wmh3UnR3SURBUUFCCi0tLS0tRU5EIFJTQSBQVUJMSUMgS0VZLS0tLS0K")
+	if err != nil {
+		t.Error(err)
+	}
+	psg.publicKey = pk
 	user, err := psg.AuthenticateRequest(req)
 	if err != nil {
 		t.Error("unexpected error authenticating request:", err)
@@ -41,10 +44,16 @@ func TestInvalidAuthenticateRequest(t *testing.T) {
 	// Add a VALID authentication token to the request
 	req.Header.Add("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwYXNzYWdlIiwic3ViIjoiQ2NGa0djWXdLTkxJcEFtWkpIdnQifQ.T0gDJTB0bd-DF6EgEO-PjKk2tbEm9ZN3qVjDeFLB1aGWnm9TYlkvxmAHtYiYmhLkRTvaO_eQIFHToQgIuRwQJlMuZyuzsISmuJCtY9dds-9q3WSoWiaiaupO6ljOSI-k31pAAx_SIaDgdl2IgH3pKLUtDZ29eZxT1HZpwFI_NJ0i_2rnrfWyDXaovgOlxTPlXW4b-GBxsPNzxLTF6-wHSMkP0hjzaLgxyWpuapG5SFIGVKBU2PtdFhChrAv-7Lk_5WxBy-SAsccrJUcYTXgVJcCIC3_rSeKlhnkbYcfQwJO9PkEq2fSCO9pt_v2LUgtC1TeMp2JcMC-6qLe2QQ44uw")
 
-	// Set a VALID public key as a properly configured server would
-	os.Setenv("PASSAGE_PUBLIC_KEY", "LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1JSUJDZ0tDQVFFQTVrZGlybnk1WnFjZ2NxcTg3YklMamtycFI3Ly9FRXRHR3lVL0xESkwxSnU2THRyaVNJemkKdzVPY2h4RkFKb25OUWhwd3dwKzNoT05OWWpKeFY1WWpibWlsc2ZMNWxsajJzZlJxY3lTMjBlZFZ2RDJqZ3hYWQpvT1g2SzU5dWtNQllzU0ZCcWtzM0UvYmNxd2YxN0U2bEIwVFhNY3NYUEtBNUE2QlJoMUVQSGtvQjlKWHJiS3hXCnhGT3JsRkJSRnJMVncvVmNUYnM3SXZRSGU4c3kxdlZoZGlkSng5R095MXVaQVlPL2dTcVNGNFRYd2RaaXJBQ1MKMjJFQlJFSSsrbWh1WWR3aWpaL3lZU1JwRFM4V0h1aVVMYjJrYnhVNUhoVG1jcnNrSjNjeEdnNHM1ZFA0V0YvZwpoMUNnYmdUczQ0dzFPVnUxdkNpNmMxQTQxMng3Wmh3UnR3SURBUUFCCi0tLS0tRU5EIFJTQSBQVUJMSUMgS0VZLS0tLS0K")
+	psg, err := New("UKbRUx")
+	if err != nil {
+		t.Error(err)
+	}
 
-	psg := passage.New()
+	pk, err := getRSAPublicKey("LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1JSUJDZ0tDQVFFQTVrZGlybnk1WnFjZ2NxcTg3YklMamtycFI3Ly9FRXRHR3lVL0xESkwxSnU2THRyaVNJemkKdzVPY2h4RkFKb25OUWhwd3dwKzNoT05OWWpKeFY1WWpibWlsc2ZMNWxsajJzZlJxY3lTMjBlZFZ2RDJqZ3hYWQpvT1g2SzU5dWtNQllzU0ZCcWtzM0UvYmNxd2YxN0U2bEIwVFhNY3NYUEtBNUE2QlJoMUVQSGtvQjlKWHJiS3hXCnhGT3JsRkJSRnJMVncvVmNUYnM3SXZRSGU4c3kxdlZoZGlkSng5R095MXVaQVlPL2dTcVNGNFRYd2RaaXJBQ1MKMjJFQlJFSSsrbWh1WWR3aWpaL3lZU1JwRFM4V0h1aVVMYjJrYnhVNUhoVG1jcnNrSjNjeEdnNHM1ZFA0V0YvZwpoMUNnYmdUczQ0dzFPVnUxdkNpNmMxQTQxMng3Wmh3UnR3SURBUUFCCi0tLS0tRU5EIFJTQSBQVUJMSUMgS0VZLS0tLS0K")
+	if err != nil {
+		t.Error(err)
+	}
+	psg.publicKey = pk
 	_, err = psg.AuthenticateRequest(req)
 	if err == nil {
 		t.Error("an invalid request was successfully authenticated")
