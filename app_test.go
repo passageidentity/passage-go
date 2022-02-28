7a6056de3eafb7d3c4df37998e6c26c5c00ed0a5
Chris Loper <chrisloper@utexas.edu>
2022-02-28 08:30:28 -0600

    update typo

--- app_test.go
+++ app_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateMagiLink(t *testing.T) {
+func TestCreateMagicLink(t *testing.T) {
 	psg, err := passage.New(passageAppID, &passage.Config{
 		APIKey: passageApiKey, // An API_KEY environment variable is required for testing.
 	})
