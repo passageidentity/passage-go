b67792097093386c667d940327d859b3dc7e5e32
Chris Tran <ctran88@users.noreply.github.com>
2024-10-24 12:04:08 -0500

    fix: updates jwx library to use its thread-safe jwks cache (#88)
    * fix: updates jwx library to use its thread-safe jwks cache
    
    * ci: adds race detector test to ci and replaces redundant test steps with a workflow call
    
    * chore: removes unnecessary comment

--- app_test.go
+++ app_test.go
@@ -1,6 +1,7 @@
 package passage_test
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/passageidentity/passage-go"
@@ -37,4 +38,33 @@ func TestGetApp(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, PassageAppID, appInfo.ID)
 
+}
+
+func TestAppNewJWKSCache(t *testing.T) {
+	psg, err := passage.New(PassageAppID, &passage.Config{
+		APIKey: PassageApiKey, // An API_KEY environment variable is required for testing.
+	})
+	require.Nil(t, err)
+	assert.NotNil(t, psg.JWKS)
+}
+
+// should be run with the -race flag, i.e. `go test -race -run TestAppJWKSCacheWriteConcurrency`
+func TestAppJWKSCacheWriteConcurrency(t *testing.T) {
+	goRoutineCount := 2
+
+	var wg sync.WaitGroup
+	wg.Add(goRoutineCount)
+
+	for i := 0; i < goRoutineCount; i++ {
+		go func() {
+			defer wg.Done()
+
+			_, err := passage.New(PassageAppID, &passage.Config{
+				APIKey: PassageApiKey, // An API_KEY environment variable is required for testing.
+			})
+			require.Nil(t, err)
+		}()
+	}
+
+	wg.Wait()
 }
