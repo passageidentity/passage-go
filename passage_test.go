94ce141d65cd7e010ebaf547c5450d1a8959348f
Chris Tran <chris.tran@agilebits.com>
2024-11-22 17:36:09 -0600

    test: adds error code to assertions

--- passage_test.go
+++ passage_test.go
@@ -45,29 +45,32 @@ func TestMain(m *testing.M) {
 
 func userNotFoundAsserts(t *testing.T, err error) {
 	splitError := strings.Split(err.Error(), ", ")
-	assert.Len(t, splitError, 4)
+	assert.Len(t, splitError, 5)
 	assert.Equal(t, "Passage Error - message: User not found", splitError[0])
 	assert.Equal(t, "status_code: 404", splitError[1])
 	assert.Equal(t, "status_text: 404 Not Found", splitError[2])
 	assert.Equal(t, "error: User not found", splitError[3])
+	assert.Equal(t, "error_code: user_not_found", splitError[4])
 }
 
 func unauthorizedAsserts(t *testing.T, err error) {
 	splitError := strings.Split(err.Error(), ", ")
-	assert.Len(t, splitError, 4)
+	assert.Len(t, splitError, 5)
 	assert.Equal(t, "Passage Error - message: Invalid access token", splitError[0])
 	assert.Equal(t, "status_code: 401", splitError[1])
 	assert.Equal(t, "status_text: 401 Unauthorized", splitError[2])
 	assert.Equal(t, "error: Invalid access token", splitError[3])
+	assert.Equal(t, "error_code: invalid_access_token", splitError[4])
 }
 
 func badRequestAsserts(t *testing.T, err error, errorText string) {
 	splitError := strings.Split(err.Error(), ", ")
-	assert.Len(t, splitError, 4)
+	assert.Len(t, splitError, 5)
 	assert.Equal(t, "Passage Error - message: "+errorText, splitError[0])
 	assert.Equal(t, "status_code: 400", splitError[1])
 	assert.Equal(t, "status_text: 400 Bad Request", splitError[2])
 	assert.Equal(t, "error: "+errorText, splitError[3])
+	assert.Equal(t, "error_code: invalid_request", splitError[4])
 }
 
 func passageUserNotFoundAsserts(t *testing.T, err error) {
