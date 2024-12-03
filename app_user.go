5b1772ce01c93e89040d588d7bf4be8746c2db11
Chris Tran <ctran88@users.noreply.github.com>
2024-12-03 11:29:55 -0600

    refactor: swaps implementation from deprecated methods to new methods (#112)

--- app_user.go
+++ app_user.go
@@ -1,213 +1,219 @@
 package passage
 
-import "errors"
+import (
+	"errors"
+)
 
-type PassageUser = User
-type appUser struct {
-	app App
-}
-
-func newAppUser(app App) *appUser {
-	appUser := appUser{
-		app: app,
-	}
-
-	return &appUser
-}
-
-// Get gets a user using their userID
+// GetUser gets a user using their userID
 // returns user on success, error on failure
-func (a *appUser) Get(userID string) (*PassageUser, error) {
-	user, err := a.app.GetUser(userID)
+//
+// Deprecated: Use `Passage.User.Get` instead.
+func (a *App) GetUser(userID string) (*User, error) {
+	user, err := a.User.Get(userID)
 	if err != nil {
-		var e Error
-		if errors.As(err, &e) {
-			return user, PassageError{
-				Message:    e.Message,
-				StatusCode: e.StatusCode,
-				ErrorCode:  e.ErrorCode,
+		var passageError PassageError
+		if errors.As(err, &passageError) {
+			return user, Error{
+				ErrorText:  passageError.Message,
+				ErrorCode:  passageError.ErrorCode,
+				Message:    passageError.Message,
+				StatusCode: passageError.StatusCode,
 			}
 		}
-
-		return user, err
 	}
 
-	return user, nil
+	return user, err
 }
 
-// GetByIdentifier gets a user using their identifier
+// GetUserByIdentifier gets a user using their identifier
 // returns user on success, error on failure
-func (a *appUser) GetByIdentifier(identifier string) (*PassageUser, error) {
-	user, err := a.app.GetUserByIdentifier(identifier)
+//
+// Deprecated: Use `Passage.User.GetByIdentifier` instead.
+func (a *App) GetUserByIdentifier(identifier string) (*User, error) {
+	user, err := a.User.GetByIdentifier(identifier)
 	if err != nil {
-		var e Error
-		if errors.As(err, &e) {
-			return user, PassageError{
-				Message:    e.Message,
-				StatusCode: e.StatusCode,
-				ErrorCode:  e.ErrorCode,
+		var passageError PassageError
+		if errors.As(err, &passageError) {
+			return user, Error{
+				ErrorText:  passageError.Message,
+				ErrorCode:  passageError.ErrorCode,
+				Message:    passageError.Message,
+				StatusCode: passageError.StatusCode,
 			}
 		}
-
-		return user, err
 	}
 
-	return user, nil
+	return user, err
 }
 
-// Activate activates a user using their userID
+// ActivateUser activates a user using their userID
 // returns user on success, error on failure
-func (a *appUser) Activate(userID string) (*PassageUser, error) {
-	user, err := a.app.ActivateUser(userID)
+//
+// Deprecated: Use `Passage.User.Activate` instead.
+func (a *App) ActivateUser(userID string) (*User, error) {
+	user, err := a.User.Activate(userID)
 	if err != nil {
-		var e Error
-		if errors.As(err, &e) {
-			return user, PassageError{
-				Message:    e.Message,
-				StatusCode: e.StatusCode,
-				ErrorCode:  e.ErrorCode,
+		var passageError PassageError
+		if errors.As(err, &passageError) {
+			return user, Error{
+				ErrorText:  passageError.Message,
+				ErrorCode:  passageError.ErrorCode,
+				Message:    passageError.Message,
+				StatusCode: passageError.StatusCode,
 			}
 		}
-
-		return user, err
 	}
 
-	return user, nil
+	return user, err
 }
 
-// Deactivate deactivates a user using their userID
+// DeactivateUser deactivates a user using their userID
 // returns user on success, error on failure
-func (a *appUser) Deactivate(userID string) (*PassageUser, error) {
-	user, err := a.app.DeactivateUser(userID)
+//
+// Deprecated: Use `Passage.User.Deactivate` instead.
+func (a *App) DeactivateUser(userID string) (*User, error) {
+	user, err := a.User.Deactivate(userID)
 	if err != nil {
-		var e Error
-		if errors.As(err, &e) {
-			return user, PassageError{
-				Message:    e.Message,
-				StatusCode: e.StatusCode,
-				ErrorCode:  e.ErrorCode,
+		var passageError PassageError
+		if errors.As(err, &passageError) {
+			return user, Error{
+				ErrorText:  passageError.Message,
+				ErrorCode:  passageError.ErrorCode,
+				Message:    passageError.Message,
+				StatusCode: passageError.StatusCode,
 			}
 		}
-
-		return user, err
 	}
 
-	return user, nil
+	return user, err
 }
 
-// Update receives an UpdateBody struct, updating the corresponding user's attribute(s)
+// UpdateUser receives an UpdateBody struct, updating the corresponding user's attribute(s)
 // returns user on success, error on failure
-func (a *appUser) Update(userID string, updateBody UpdateBody) (*PassageUser, error) {
-	user, err := a.app.UpdateUser(userID, updateBody)
+//
+// Deprecated: Use `Passage.User.Update` instead.
+func (a *App) UpdateUser(userID string, updateBody UpdateBody) (*User, error) {
+	user, err := a.User.Update(userID, updateBody)
 	if err != nil {
-		var e Error
-		if errors.As(err, &e) {
-			return user, PassageError{
-				Message:    e.Message,
-				StatusCode: e.StatusCode,
-				ErrorCode:  e.ErrorCode,
+		var passageError PassageError
+		if errors.As(err, &passageError) {
+			return user, Error{
+				ErrorText:  passageError.Message,
+				ErrorCode:  passageError.ErrorCode,
+				Message:    passageError.Message,
+				StatusCode: passageError.StatusCode,
+			}
+		}
+	}
+
+	return user, err
+}
+
+// DeleteUser receives a userID (string), and deletes the corresponding user
+// returns true on success, false and error on failure (bool, err)
+//
+// Deprecated: Use `Passage.User.Delete` instead.
+func (a *App) DeleteUser(userID string) (bool, error) {
+	if err := a.User.Delete(userID); err != nil {
+		var passageError PassageError
+		if errors.As(err, &passageError) {
+			return false, Error{
+				ErrorText:  passageError.Message,
+				ErrorCode:  passageError.ErrorCode,
+				Message:    passageError.Message,
+				StatusCode: passageError.StatusCode,
 			}
 		}
 
-		return user, err
+		return false, err
 	}
 
-	return user, nil
+	return true, nil
 }
 
-// Delete deletes a user by their user string
-// returns error on failure
-func (a *appUser) Delete(userID string) error {
-	if _, err := a.app.DeleteUser(userID); err != nil {
-		var e Error
-		if errors.As(err, &e) {
-			return PassageError{
-				Message:    e.Message,
-				StatusCode: e.StatusCode,
-				ErrorCode:  e.ErrorCode,
-			}
-		}
-
-		return err
-	}
-
-	return nil
-}
-
-// Create receives a CreateUserBody struct, creating a user with provided values
+// CreateUser receives a CreateUserBody struct, creating a user with provided values
 // returns user on success, error on failure
-func (a *appUser) Create(createUserBody CreateUserBody) (*PassageUser, error) {
-	user, err := a.app.CreateUser(createUserBody)
+//
+// Deprecated: Use `Passage.User.Create` instead.
+func (a *App) CreateUser(createUserBody CreateUserBody) (*User, error) {
+	user, err := a.User.Create(createUserBody)
 	if err != nil {
-		var e Error
-		if errors.As(err, &e) {
-			return user, PassageError{
-				Message:    e.Message,
-				StatusCode: e.StatusCode,
-				ErrorCode:  e.ErrorCode,
+		var passageError PassageError
+		if errors.As(err, &passageError) {
+			return user, Error{
+				ErrorText:  passageError.Message,
+				ErrorCode:  passageError.ErrorCode,
+				Message:    passageError.Message,
+				StatusCode: passageError.StatusCode,
 			}
 		}
-
-		return user, err
 	}
 
-	return user, nil
+	return user, err
 }
 
-// ListDevices lists a user's devices
+// ListUserDevices lists a user's devices
 // returns a list of devices on success, error on failure
-func (a *appUser) ListDevices(userID string) ([]WebAuthnDevices, error) {
-	devices, err := a.app.ListUserDevices(userID)
+//
+// Deprecated: Use `Passage.User.ListDevices` instead.
+func (a *App) ListUserDevices(userID string) ([]WebAuthnDevices, error) {
+	devices, err := a.User.ListDevices(userID)
 	if err != nil {
-		var e Error
-		if errors.As(err, &e) {
-			return devices, PassageError{
-				Message:    e.Message,
-				StatusCode: e.StatusCode,
-				ErrorCode:  e.ErrorCode,
+		var passageError PassageError
+		if errors.As(err, &passageError) {
+			return devices, Error{
+				ErrorText:  passageError.Message,
+				ErrorCode:  passageError.ErrorCode,
+				Message:    passageError.Message,
+				StatusCode: passageError.StatusCode,
 			}
 		}
-
-		return devices, err
 	}
 
-	return devices, nil
+	return devices, err
 }
 
-// RevokeDevice gets a user using their userID
-// returns error on failure
-func (a *appUser) RevokeDevice(userID, deviceID string) error {
-	if _, err := a.app.RevokeUserDevice(userID, deviceID); err != nil {
-		var e Error
-		if errors.As(err, &e) {
-			return PassageError{
-				Message:    e.Message,
-				StatusCode: e.StatusCode,
-				ErrorCode:  e.ErrorCode,
+// RevokeUserDevice gets a user using their userID
+// returns a true success, error on failure
+//
+// Deprecated: Use `Passage.User.RevokeDevice` instead.
+func (a *App) RevokeUserDevice(userID, deviceID string) (bool, error) {
+	if err := a.User.RevokeDevice(userID, deviceID); err != nil {
+		var passageError PassageError
+		if errors.As(err, &passageError) {
+			return false, Error{
+				ErrorText:  passageError.Message,
+				ErrorCode:  passageError.ErrorCode,
+				Message:    passageError.Message,
+				StatusCode: passageError.StatusCode,
 			}
 		}
 
-		return err
+		return false, err
 	}
 
-	return nil
+	return true, nil
 }
 
-// RevokeRefreshTokens revokes a users refresh tokens
-// returns error on failure
-func (a *appUser) RevokeRefreshTokens(userID string) error {
-	if _, err := a.app.SignOut(userID); err != nil {
-		var e Error
-		if errors.As(err, &e) {
-			return PassageError{
-				Message:    e.Message,
-				StatusCode: e.StatusCode,
-				ErrorCode:  e.ErrorCode,
+// Signout revokes a users refresh tokens
+// returns true on success, error on failure
+//
+// Deprecated: Use `Passage.User.RevokeRefreshTokens` instead.
+func (a *App) SignOut(userID string) (bool, error) {
+	if err := a.User.RevokeRefreshTokens(userID); err != nil {
+
+		var passageError PassageError
+		if errors.As(err, &passageError) {
+			return false, Error{
+				ErrorText:  passageError.Message,
+				ErrorCode:  passageError.ErrorCode,
+				Message:    passageError.Message,
+				StatusCode: passageError.StatusCode,
 			}
 		}
 
-		return err
+		return false, err
 	}
 
-	return nil
+	return true, nil
 }
--- passage_test.go
+++ passage_test.go
@@ -45,54 +45,68 @@ func TestMain(m *testing.M) {
 
 func userNotFoundAsserts(t *testing.T, err error) {
 	splitError := strings.Split(err.Error(), ", ")
-	assert.Len(t, splitError, 5)
+	assert.Len(t, splitError, 4)
 	assert.Equal(t, "Passage Error - message: User not found", splitError[0])
 	assert.Equal(t, "status_code: 404", splitError[1])
-	assert.Equal(t, "status_text: 404 Not Found", splitError[2])
-	assert.Equal(t, "error: User not found", splitError[3])
-	assert.Equal(t, "error_code: user_not_found", splitError[4])
+	assert.Equal(t, "error: User not found", splitError[2])
+	assert.Equal(t, "error_code: user_not_found", splitError[3])
+}
+
+func couldNotFindUserByIdentifierAsserts(t *testing.T, err error) {
+	splitError := strings.Split(err.Error(), ", ")
+	assert.Len(t, splitError, 4)
+	assert.Equal(t, "Passage Error - message: Could not find user with that identifier.", splitError[0])
+	assert.Equal(t, "status_code: 404", splitError[1])
+	assert.Equal(t, "error: Could not find user with that identifier.", splitError[2])
+	assert.Equal(t, "error_code: user_not_found", splitError[3])
 }
 
 func unauthorizedAsserts(t *testing.T, err error) {
 	splitError := strings.Split(err.Error(), ", ")
-	assert.Len(t, splitError, 5)
+	assert.Len(t, splitError, 4)
 	assert.Equal(t, "Passage Error - message: Invalid access token", splitError[0])
 	assert.Equal(t, "status_code: 401", splitError[1])
-	assert.Equal(t, "status_text: 401 Unauthorized", splitError[2])
-	assert.Equal(t, "error: Invalid access token", splitError[3])
-	assert.Equal(t, "error_code: invalid_access_token", splitError[4])
+	assert.Equal(t, "error: Invalid access token", splitError[2])
+	assert.Equal(t, "error_code: invalid_access_token", splitError[3])
 }
 
 func badRequestAsserts(t *testing.T, err error, errorText string) {
 	splitError := strings.Split(err.Error(), ", ")
-	assert.Len(t, splitError, 5)
+	assert.Len(t, splitError, 4)
 	assert.Equal(t, "Passage Error - message: "+errorText, splitError[0])
 	assert.Equal(t, "status_code: 400", splitError[1])
-	assert.Equal(t, "status_text: 400 Bad Request", splitError[2])
-	assert.Equal(t, "error: "+errorText, splitError[3])
-	assert.Equal(t, "error_code: invalid_request", splitError[4])
+	assert.Equal(t, "error: "+errorText, splitError[2])
+	assert.Equal(t, "error_code: invalid_request", splitError[3])
 }
 
 func passageUserNotFoundAsserts(t *testing.T, err error) {
 	splitError := strings.Split(err.Error(), ", ")
 	assert.Len(t, splitError, 3)
-	assert.Equal(t, "Passage Error - message: User not found", splitError[0])
-	assert.Equal(t, "status_code: 404", splitError[1])
-	assert.Equal(t, "error_code: user_not_found", splitError[2])
+	assert.Equal(t, "PassageError - message: User not found", splitError[0])
+	assert.Equal(t, "errorCode: user_not_found", splitError[1])
+	assert.Equal(t, "statusCode: 404", splitError[2])
+}
+
+func passageCouldNotFindUserByIdentifierAsserts(t *testing.T, err error) {
+	splitError := strings.Split(err.Error(), ", ")
+	assert.Len(t, splitError, 3)
+	assert.Equal(t, "PassageError - message: Could not find user with that identifier.", splitError[0])
+	assert.Equal(t, "errorCode: user_not_found", splitError[1])
+	assert.Equal(t, "statusCode: 404", splitError[2])
 }
 
 func passageUnauthorizedAsserts(t *testing.T, err error) {
 	splitError := strings.Split(err.Error(), ", ")
 	assert.Len(t, splitError, 3)
-	assert.Equal(t, "Passage Error - message: Invalid access token", splitError[0])
-	assert.Equal(t, "status_code: 401", splitError[1])
-	assert.Equal(t, "error_code: invalid_access_token", splitError[2])
+	assert.Equal(t, "PassageError - message: Invalid access token", splitError[0])
+	assert.Equal(t, "errorCode: invalid_access_token", splitError[1])
+	assert.Equal(t, "statusCode: 401", splitError[2])
 }
 
 func passageBadRequestAsserts(t *testing.T, err error, message string) {
 	splitError := strings.Split(err.Error(), ", ")
 	assert.Len(t, splitError, 3)
-	assert.Equal(t, "Passage Error - message: "+message, splitError[0])
-	assert.Equal(t, "status_code: 400", splitError[1])
-	assert.Equal(t, "error_code: invalid_request", splitError[2])
+	assert.Equal(t, "PassageError - message: "+message, splitError[0])
+	assert.Equal(t, "errorCode: invalid_request", splitError[1])
+	assert.Equal(t, "statusCode: 400", splitError[2])
 }
