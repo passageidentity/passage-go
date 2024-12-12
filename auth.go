741b260801b6fc127351597c45432c71ebc9786e
Chris Tran <ctran88@users.noreply.github.com>
2024-12-12 11:10:14 -0600

    feat: reworks the new create magic link func into three separate functions (#117)

--- auth.go
+++ auth.go
@@ -10,6 +10,13 @@ import (
 	"github.com/lestrrat-go/jwx/v2/jwk"
 )
 
+type MagicLinkOptions struct {
+	Language      string
+	MagicLinkPath string
+	RedirectURL   string
+	TTL           int
+}
+
 type auth struct {
 	appID        string
 	client       *ClientWithResponses
@@ -38,18 +45,56 @@ func newAuth(appID string, client *ClientWithResponses) (*auth, error) {
 	return &auth, nil
 }
 
-// CreateMagicLink creates a Magic Link for your app.
-func (a *auth) CreateMagicLink(args CreateMagicLinkBody) (*MagicLink, error) {
-	res, err := a.client.CreateMagicLinkWithResponse(context.Background(), a.appID, args)
-	if err != nil {
-		return nil, err
+// CreateMagicLink creates a Magic Link for your app using an email address.
+func (a *auth) CreateMagicLinkWithEmail(
+	email string,
+	magicLinkType MagicLinkType,
+	send bool,
+	opts *MagicLinkOptions,
+) (*MagicLink, error) {
+	args := CreateMagicLinkBody{
+		Email:   email,
+		Channel: EmailChannel,
+		Type:    magicLinkType,
+		Send:    send,
 	}
 
-	if res.JSON201 != nil {
-		return &res.JSON201.MagicLink, nil
+	return a.createMagicLink(args, opts)
+}
+
+// CreateMagicLink creates a Magic Link for your app using an E164-formatted phone number.
+func (a *auth) CreateMagicLinkWithPhone(
+	phone string,
+	magicLinkType MagicLinkType,
+	send bool,
+	opts *MagicLinkOptions,
+) (*MagicLink, error) {
+	args := CreateMagicLinkBody{
+		Phone:   phone,
+		Channel: PhoneChannel,
+		Type:    magicLinkType,
+		Send:    send,
 	}
 
-	return nil, errorFromResponse(res.Body, res.StatusCode())
+	return a.createMagicLink(args, opts)
+}
+
+// CreateMagicLink creates a Magic Link for your app using a Passage user ID.
+func (a *auth) CreateMagicLinkWithUser(
+	userID string,
+	channel ChannelType,
+	magicLinkType MagicLinkType,
+	send bool,
+	opts *MagicLinkOptions,
+) (*MagicLink, error) {
+	args := CreateMagicLinkBody{
+		UserID:  userID,
+		Channel: channel,
+		Type:    magicLinkType,
+		Send:    send,
+	}
+
+	return a.createMagicLink(args, opts)
 }
 
 // ValidateJWT validates the JWT and returns the user ID.
@@ -78,6 +123,26 @@ func (a *auth) ValidateJWT(jwt string) (string, error) {
 	}
 
 	return userID, nil
+}
+
+func (a *auth) createMagicLink(args CreateMagicLinkBody, opts *MagicLinkOptions) (*MagicLink, error) {
+	if opts != nil {
+		args.Language = opts.Language
+		args.MagicLinkPath = opts.MagicLinkPath
+		args.RedirectURL = opts.RedirectURL
+		args.TTL = opts.TTL
+	}
+
+	res, err := a.client.CreateMagicLinkWithResponse(context.Background(), a.appID, args)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.JSON201 != nil {
+		return &res.JSON201.MagicLink, nil
+	}
+
+	return nil, errorFromResponse(res.Body, res.StatusCode())
 }
 
 func (a *auth) getPublicKey(token *jwt.Token) (interface{}, error) {
