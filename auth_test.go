86f157c066d99ca549fb511ffb9ac62da972c05f
Chris Tran <chris.tran@agilebits.com>
2024-12-19 13:56:54 -0600

    feat!: remove deprecated code and rename structs and files
    also moves some logic around to be closer to their usage

---
+++ auth_test.go
@@ -0,0 +1,36 @@
+package passage_test
+
+import (
+	"os"
+	"sync"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/passageidentity/passage-go"
+	"github.com/stretchr/testify/require"
+)
+
+// should be run with the -race flag, i.e. `go test -race -run TestAppJWKSCacheWriteConcurrency`
+func TestAppJWKSCacheWriteConcurrency(t *testing.T) {
+	err := godotenv.Load(".env")
+	require.Nil(t, err)
+
+	appID := os.Getenv("PASSAGE_APP_ID")
+	apiKey := os.Getenv("PASSAGE_API_KEY")
+
+	goRoutineCount := 2
+
+	var wg sync.WaitGroup
+	wg.Add(goRoutineCount)
+
+	for i := 0; i < goRoutineCount; i++ {
+		go func() {
+			defer wg.Done()
+
+			_, err := passage.New(appID, apiKey)
+			require.Nil(t, err)
+		}()
+	}
+
+	wg.Wait()
+}
